9d5328e3ab5e3fd488a1ea11a4507564aa5d9421
qiujz <qiujz@hotmail.com>
2014-11-17 21:12:13 +0800

    实现系统参数查询

--- 40Bll/Hot371.Bll/Sys/SysIndustryBiz.cs
+++ 40Bll/Hot371.Bll/Sys/SysIndustryBiz.cs
@@ -8,29 +8,26 @@ using QJZ.Framework.Utility;
 
 namespace Hot371.Bll.Sys
 {
-    /// <summary>
-    /// 系统参数业务逻辑
-    /// </summary>
-    public class SysParametersBiz
+    public class SysIndustryBiz
     {
         /// <summary>
         /// 缓存key
         /// </summary>
-        private static readonly string cacheKey = "Hot371.AllParameters";
+        private static readonly string cacheKey = "Hot371.AllIndustry";
 
         /// <summary>
         /// 获取全部放缓存 1小时
         /// </summary>
         /// <returns></returns>
-        public IEnumerable<SysParameters> GetAll()
+        public IEnumerable<SysIndustry> GetAll()
         {
-            var cache = Caching.Get(cacheKey) as IEnumerable<SysParameters>;
+            var cache = Caching.Get(cacheKey) as IEnumerable<SysIndustry>;
             if (cache != null)
             {
                 return cache;
             }
 
-            var resp = new SysParametersRespository();
+            var resp = new SysIndustryRespository();
             var all = resp.GetAll();
             try
             {
@@ -40,18 +37,6 @@ namespace Hot371.Bll.Sys
             {
             }
             return all;
-        }
-
-        /// <summary>
-        /// 根据分组获取
-        /// </summary>
-        /// <param name="gId"></param>
-        /// <returns></returns>
-        public List<SysParameters> GetByGroup(int gId)
-        {
-            var all = GetAll();
-            var paras = all.Where(p => p.GroupId == gId).ToList();
-            return paras;
         }
     }
 }
