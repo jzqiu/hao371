1c6d4580d493b81e774e8109470be07c0f454e7e
qjz07254 <qjz07254@LY.com>
2014-11-18 14:11:47 +0800

    二维码

--- 40Bll/Hot371.Bll/Sys/SysIndustryBiz.cs
+++ 40Bll/Hot371.Bll/Sys/SysIndustryBiz.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using Hot371.Dal.Sys;
 using Hot371.Model;
+using Hot371.ViewModel;
 using QJZ.Framework.Utility;
 
 namespace Hot371.Bll.Sys
@@ -43,6 +44,30 @@ namespace Hot371.Bll.Sys
             {
             }
             return all;
+        }
+
+        /// <summary>
+        /// 获取行业及其职能
+        /// </summary>
+        /// <returns></returns>
+        public List<IndustryFunctionsVM> GetIndustryFunctions()
+        {
+            var industries = GetAll();
+            var funcBiz = new SysIFunctionBiz();
+            var functions = funcBiz.GetAll();
+            var result = from r in industries
+                orderby r.Sort
+                select new IndustryFunctionsVM
+                {
+                    IId = r.IId,
+                    IName = r.IName,
+                    ImgUrl = r.ImgUrl,
+                    Functions = from f in functions
+                        where f.IId == r.IId
+                        orderby f.Sort
+                        select f
+                };
+            return result.ToList();
         }
     }
 }
